731c73366fcd918a7bbf09d492676882611ff678
mwk000 <hy5600mwk000@hotmail.com>
2023-10-18 21:22:18 +0800

    摸鱼了

--- Assets/Script/Card/StartEvent.cs
+++ Assets/Script/Card/StartEvent.cs
@@ -12,25 +12,40 @@ namespace Script.Card
     //进入战斗场景触发的事件
     public class StartEvent : MonoBehaviour
     {
-        private PrefabPool _cardPool;
-        private List<CardInfo> _handCards = new List<CardInfo>();
-        private Queue<CardInfo> _deck = new Queue<CardInfo>();
-       
+        
+
+        private PrefabPool _cardPool;//card对象池
+        private List<CardInfo> _handCards = new List<CardInfo>();//手牌
+        private Queue<CardInfo> _deck = new Queue<CardInfo>();//卡组
+        private int _init_handCards_Num = 7;
+        private Vector3 deck_icon_position = GameObject.Find("deck").transform.localPosition;//卡组的位置，抽卡从这里出现
+        
         private void Start()
         {
-            //deck乱序转队列，形成卡组，第一次洗牌
+            //初始化对象池
+            BasePoolMap basePoolMap = BasePoolMap.GetInstance();
+            _cardPool = basePoolMap.GetPoolByPrefabPath("card", transform, 10, 30,true, null, null, null, null);
+            /*deck乱序转队列，形成卡组，第一次洗牌*/
             //卡组列表
             List<CardInfo> deckCards = AllParamEntity.GetInstance().GETDeckCards();
             ShuffleDeck(deckCards);
-            //抽卡
+            //获取手牌数据
             DrawInitCards();
             
         }
+        
+        
+        private void Update()
+        {
+           
+        }
+        
+        
 
         //洗牌
         private void ShuffleDeck(List<CardInfo> cardList)
         {
-           ListUtils.Shuffle(cardList);
+           ListUtil.Shuffle(cardList);
            foreach (var card in cardList)
            {
                _deck.Enqueue(card);
@@ -38,19 +53,22 @@ namespace Script.Card
            
         }
         
-        //初始抽卡 7张
+        //初始抽卡 
         private void DrawInitCards()
         {
-            BasePoolMap basePoolMap = BasePoolMap.GetInstance();
-            _cardPool = basePoolMap.GetPoolByPrefabPath("card", transform, 10, 30,true, null, null, null, null);
-            
+            //后端
+            for (int i = 0; i < _init_handCards_Num; i++)
+            {
+                CardInfo card = _deck.Dequeue();
+                _handCards.Add(card);
+            }
+            //前端
+            var gameObjectCard = _cardPool.Get();
+            //设置卡牌初始位置
+            gameObjectCard.transform.localPosition = deck_icon_position;
+            //移动卡牌到手牌区
+            gameObjectCard.GetComponent<Rigidbody2D>().velocity = Vector2.left;
         }
 
-        //创建card物体到指定位置
-        private void CreateCardInPosition()
-        {
-            
-        }
-       
     }
 }
--- Assets/Script/Util/ListUtil.cs
+++ Assets/Script/Util/ListUtil.cs
@@ -7,23 +7,23 @@ namespace Script.Util
     {
         private static Random r;
 
-        public static void shuffle<T>(List<T> list)
+        public static void Shuffle<T>(List<T> list)
         {
             Random rnd = r;
             if (rnd == null)
             {
                 r = rnd = new Random();
             }
-            shuffle(list,rnd);
+            Shuffle(list,rnd);
         }
 
-        public static void shuffle<T>(List<T> list, Random rnd)
+        public static void Shuffle<T>(List<T> list, Random rnd)
         {
             int size = list.Count;
             T[] arr = list.ToArray();
             for (int i = size; i > 1; i--)
             {
-                swap(arr, i-1, rnd.Next(i));
+                Swap(arr, i-1, rnd.Next(i));
             }
             
             for (int i = 0; i < arr.Length; i++)
@@ -32,7 +32,7 @@ namespace Script.Util
             }
         }
         
-        private static void swap<T>(T[] arr, int i, int j) {
+        private static void Swap<T>(T[] arr, int i, int j) {
             T tmp = arr[i];
             arr[i] = arr[j];
             arr[j] = tmp;
