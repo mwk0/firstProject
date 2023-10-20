6c45f8e577b764b00c72d6dc84147b04f96ef650
mwk0 <hy5600mwk000@hotmail.com>
2023-10-20 16:56:10 +0800

    用Resource.loadAll加载卡牌图鉴作为全局参数

--- Assets/Script/Entity/AllParamEntity.cs
+++ Assets/Script/Entity/AllParamEntity.cs
@@ -6,12 +6,17 @@ namespace Script.Entity
     //全局参数
     public class AllParamEntity:SingletonBaseManager<AllParamEntity>
     {
+        //全量的卡牌图鉴
+        public static CardInfo[] _allCardInfo;
         //卡组
         private List<CardInfo> _deckCards = new List<CardInfo>();
+        
 
         public List<CardInfo> GETDeckCards()
         {
             return _deckCards;
         }
+        
+       
     }
 }
--- Assets/Script/MainTest/CardBattleStart.cs
+++ Assets/Script/MainTest/CardBattleStart.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Script.Entity;
 using Script.Pub.Singletonbase;
 using UnityEngine;
+using Object = System.Object;
+using Random = System.Random;
 
 namespace Script.MainTest
 {
@@ -10,12 +13,19 @@ namespace Script.MainTest
     {
         private void Start()
         {
-            //开始游戏
+            //全局参数
             AllParamEntity allParams = AllParamEntity.GetInstance();
+            //加载所有的卡牌图鉴
+            CardInfo[] allCardScripteObjects = Resources.LoadAll("ScripteObject", typeof(CardInfo)).Cast<CardInfo>().ToArray();
+            AllParamEntity._allCardInfo = allCardScripteObjects;
+            
+            //开始游戏(测试：从所有牌里随机挑20张)
+            int allCardCount = AllParamEntity._allCardInfo.Length;
             List<CardInfo> deck = allParams.GETDeckCards();
+            Random random = new Random();
             for (int i = 0; i < 20; i++)
             {
-                
+                deck.Add(AllParamEntity._allCardInfo[random.Next(allCardCount)]);
             }
             Debug.Log(allParams.GETDeckCards().Count);
         }
